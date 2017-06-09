3143a438f3f7cf69f01a2f7ce8dae53f8cc8d31a
yonger <1046788379@qq.com>
2017-06-09 23:37:10 +0800

    完成添加讲师功能&完成注销与开启讲师功能&完成查看讲师列表功能

--- js/teacher/teacher_add.js
+++ js/teacher/teacher_add.js
@@ -1,3 +1,17 @@
-define(['aside'], function(undefined) {
-	
+define(['aside', 'header', 'nprogress', 'jquery', 'loading'], function(undefined, undefined, nprogress, $, undefined) {
+	//添加讲师功能
+    (function(){
+        $('#teacher-add-edit').on('submit',function(){
+            $.post('/v6/teacher/add',$(this).serialize(),function(data){
+                // if(data.code ==200){
+                    location.href = '/html/teacher/teacher_list.html';
+                // }
+            })
+            return false;
+        })
+    })();
+    
+
+
+    nprogress.done();
 });
--- js/teacher/teacher_list.js
+++ js/teacher/teacher_list.js
@@ -1,15 +1,52 @@
-define(['aside','header','nprogress','jquery','loading','template'], function(undefined,undefined,nprogress,$,undefined,template) {
-	//渲染讲师列表
-    (function(){
+define(['bootstrap', 'aside', 'header', 'nprogress', 'jquery', 'loading', 'template'], function (undefined, undefined, undefined, nprogress, $, undefined, template) {
+    //渲染讲师列表
+    (function () {
         //1.获取数据
         //2.渲染列表 
-         $.get('/v6/teacher',function(data){
-             if(data.code===200){
-                 $('.table-teacher-list').append(template('teacher-list-tpl',data))
-             }
-           
+        $.get('/v6/teacher', function (data) {
+            if (data.code === 200) {
+                $('.table-teacher-list').append(template('teacher-list-tpl', data))
+            }
+
         })
     })();
 
+    // 查看讲师列表
+    (function () {
+        //利用事件委托的方式来给动态元素绑定事件.
+        $(document).on('click', '.teacher-vue-id', function () {
+            //  $('#teacherModal').append(template('teacher-view-tpl',{}))
+            $.get('/v6/teacher/view', {
+                tc_id: ($(this).data('teacher-id'))
+            }, function (data) {
+                if(data.code===200){
+                    $('#teacherModal').append(template('teacher-view-tpl',data.result))
+                }
+            })
+        })
+    })();
+
+    //讲师注销与开启功能
+    (function(){
+        $(document).on('click','.teacher-handle',function(){
+            var self = this;//为了获得动态生成的元素,在回调里面可以用
+            // console.log(self);
+            $.post('/v6/teacher/handle',{
+                tc_id:$(this).data('teacher-id'),
+                tc_status:$(this).data('teacher-status')
+            },function(data){
+                // console.log(1111); //请求403 禁止状态 ,进入不了回调
+                if(data.code==200){
+                    if(data.result.tc_status==0){
+                        $(self).data('teacher-status',0);//及时更新teacher的状态
+                        $(self).html('注销');
+                    }else {
+                        $(self).data('teacher-status',1);
+                        $(self).html('开启');
+                    }
+                }
+            })
+        })
+    })()
     nprogress.done();
 });
