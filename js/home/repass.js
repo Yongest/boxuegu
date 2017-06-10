f13255444723e527f39e17eae764d7ae90d80c02
yonger <1046788379@qq.com>
2017-06-10 22:31:24 +0800

    完成个人中心功能,包括数据的回写与提交&完成修改密码功能;用到js中的trigger方法控制页面自动点击事件登出功能,用到datepicker与ajaxForm插件.其中ajaxForm有ajaxForm()与ajaxsubmit()两个方法,可以不用输入提交的表单数据.datepicker有语言包,css文件,依赖jquery.

--- js/home/repass.js
+++ js/home/repass.js
@@ -1,3 +1,11 @@
-define([], function() {
-	
+define(['jquery','common','header','aside','nprogress','loading','jqueryForm'], function($,undefined,undefined,undefined,nprogress,undefined,undefined) {
+    // 修改密码功能,接口与提交方式已经在form表单中写好
+	$('#change-pass').ajaxForm(function(data){
+        //用js方式触发点击事件,让修改好密码后,跳到登录页面
+        if(data.code==200){
+            $('#logout').trigger('click');
+        }
+    })
+
+    nprogress.done();
 });
--- js/home/settings.js
+++ js/home/settings.js
@@ -1,3 +1,23 @@
-define([], function() {
-	
+define(['jquery','common','header','aside','nprogress','loading','template','jqueryForm','datepicker','datepickerCN'], function($,undefined,undefined,undefined,nprogress,undefined,template,undefined,undefined,undefined) {
+    //个人中心数据回显
+	$.get('/v6/teacher/profile',function(data){
+        if(data.code==200){
+            $('#settings-item').html(template('settings-form-tpl'),data);
+            //模板渲染之后再调用方法
+            $('.datepicker-input').datepicker({
+                language:'zh-CN',
+                endDate:new Date(),
+                format:'yyyy-mm-dd'
+            })
+            modify();
+        }
+    });
+
+    // 个人中心数据提交
+    function modify(){
+        $('#settings-form').ajaxForm(function(){
+        location.reload();
+    })
+    }
+    nprogress.done();
 });
