8bdc44f9277670baa10592eabf46a290eeb2434f
yonger <1046788379@qq.com>
2017-06-12 20:49:27 +0800

    完成地区三级联动功能,使用了find("option:selected")伪类选择器.调用region()方法,传入对象,输入.json文件.配置selected的id以及data-id

--- js/home/settings.js
+++ js/home/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','common','header','aside','nprogress','loading','template','jqueryForm','datepicker','datepickerCN','ckeditor'], function($,undefined,undefined,undefined,nprogress,undefined,template,undefined,undefined,undefined,ckeditor) {
+define(['jquery','common','header','aside','nprogress','loading','template','jqueryForm','datepicker','datepickerCN','ckeditor','jqueryRegion'], function($,undefined,undefined,undefined,nprogress,undefined,template,undefined,undefined,undefined,ckeditor,undefined) {
     
     var edit=null;//全局变量.ckeditor 的实例.
     //个人中心数据回显
@@ -11,11 +11,14 @@ define(['jquery','common','header','aside','nprogress','loading','template','jqu
                 language:'zh-CN',
                 endDate:new Date(),
                 format:'yyyy-mm-dd'
-            })
+            });
             edit = ckeditor.replace('ckeditor',{
                 toolbarGroups:{//配置文件
                     name:'styles'
                 }
+            });
+            $('#region').region({
+                url:'/lib/jquery-region/region.json'
             })
         }
     });
@@ -32,12 +35,18 @@ define(['jquery','common','header','aside','nprogress','loading','template','jqu
          $('#settings-form').on('submit',function(){
             //  数据发送前调用一下这个方法,把富文本里面的值传给文本域
              edit.updateElement();
-            $(this).ajaxSubmit(function(data){
-                if(data.code ==200){
-                    location.reload();
+            $(this).ajaxSubmit({
+                // 需要在表单提交时,额外增加一个tc_hometown的值
+                data:{
+                    tc_hometown:$('#p').find('option:seledted')+'|'+$('#c').find('option:seledted')+'|'+$('#d').find('option:seledted')
+                },
+                success:function(data){
+                    if(data.code==200){
+                        location.reload();
+                    }
                 }
             })
-            阻止表单默认行为,自动刷新
+            // 阻止表单默认行为,自动刷新
             return false;
          })
     }
