d494ac5c72d62fab26ebbbd8233d3ab5a8d39bfb
yonger <1046788379@qq.com>
2017-06-14 12:20:31 +0800

    完成课程添加第一步,重点是不适用插件完成了select的二级联动功能,还有template的使用,$value.something.利用uploadify这个插件完成了图片上传功能.需要一个input文件类型的表单.另外文件上传需要等待页面渲染完毕.插件里面有很多配置,比如添加样式,增加参数,height,width,等,详情见www.uploadify.com flash版本

--- js/course/course_add.js
+++ js/course/course_add.js
@@ -1,3 +1,9 @@
-define([], function() {
-	
+define(['jquery', 'common', 'header', 'aside', 'nprogress', 'loading', 'template', 'jqueryForm'], function ($, common, undefined, undefined, nprogress, undefined, template, undefined) {
+    
+    $('#course-create-form').ajaxForm(function (data) {
+        //使页面跳转到course_add_step1.html
+        (data.code == 200) && (location.href = '/html/course/course_add_step1.html'+'?cs_id='+data.result.cs_id);
+    })
+
+    nprogress.done();
 });
--- js/course/course_add_step2.js
+++ js/course/course_add_step2.js
@@ -1,3 +1,43 @@
-define([], function() {
-	
+define(['jquery', 'common', 'header', 'aside', 'nprogress', 'loading', 'template', 'jqueryForm', 'ckeditor', 'uploadify'], function ($, common, undefined, undefined, nprogress, undefined, template, undefined, undefined, undefined) {
+
+    function initAddAside() {
+        $('.forwards a').removeClass('active').first().next().addClass('active');
+    }
+    var csId = common.parseSearch('cs_id');
+    $.get('/v6/course/picture', { cs_id: csId }, function (data) {
+        (data.code == 200) && ($('.steps').html(template('add-step2-tpl', data.result))) && (initAddAside());
+        // 文件上传功能放在这里面
+        uploadFile();
+    })
+
+
+    //文件上传功能,需要等待参数传递回来,页面渲染完毕之后,才能执行
+    function uploadFile() {
+        $('#upfile').uploadify({
+            swf: '/lib/uploadify/uploadify.swf',
+            uploadify: '/v6/uploader/cover',
+            fileObjName: 'cs_cover_original',
+            formData: { cs_id: csId },
+            buttonClass: 'btn btn-success btn-sm',
+            buttonText: '选择图片',
+            itemTemplate: '<i><i>',
+            height: '100%',
+            width: '100%',
+            onUploadSuccess: function (file, data, response) {
+                //    一般用到JSON.parse()这个方法,都要加上try .. catch...,防止报错
+                try {
+                    data = JSON.parse(data);
+                } catch (e) {
+                    data = {};
+                }
+                if (data.code == 200) {
+                    $('.preview img').attr('src', JSON.parse(data.result.path));
+                    location.href = ('/html/course/course_add_step3.html?cs_id=' + csId);
+                }
+
+            }
+        })
+    }
+    nprogress.done();
+
 });
